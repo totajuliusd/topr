a27e1981f217ac0d98390b4c47586d00ce673377
Þórhildur Júlíusdóttir <thorhildur@wuxinextcode.com>
2021-06-17 21:15:59 +0000

    Gene desert fix: when no genes/exons are returned for the regionplot

--- R/geneplot.R
+++ R/geneplot.R
@@ -1,6 +1,9 @@
 
-geneplot=function(genes, xmin, xmax, label_size=3.5,vline=NULL){
-
+geneplot=function(genes,chr, xmin, xmax, label_size=3.5,vline=NULL){
+  chr=gsub("chr", "", chr)
+  level_count=0
+  #first check whether there are any genes/exons in the region
+  if(nrow(genes)>0){
   # to=as.numeric(xmax)
   #from=as.numeric(xmin)
   min_space_between_genes=as.numeric((xmax-xmin)*0.1)
@@ -14,7 +17,7 @@ geneplot=function(genes, xmin, xmax, label_size=3.5,vline=NULL){
 
   #  print(paste("Max range in the gene plot function ", to))
   current_level=1
-  level_count=0
+
   gene_struct = data.frame(gene_end=integer(),level=integer(),gene=character())
   chr=gsub("chr","", unique(genes$chrom))
   for(i in 1:length(genes$gene_symbol)){
@@ -51,17 +54,22 @@ geneplot=function(genes, xmin, xmax, label_size=3.5,vline=NULL){
     y2=append(current_level+0.20,y2)
     genes$y[i]=current_level+0.25
   }
-  shades=data.frame(x1=x1, x2=x2, y1=y1,y2=y2,biotype=biotype)
-  p1=ggplot(data =  genes)+theme_bw()
-  p1=p1+geom_text(data=genes, aes(x=gene_start,y=as.numeric(y),label=gene_symbol),hjust=0, vjust=0, size=label_size)
+  }
+  #Do the plotting
 
+  p1=ggplot(data =  genes)+theme_bw()
+
+  if(nrow(genes)>0){
+    shades=data.frame(x1=x1, x2=x2, y1=y1,y2=y2,biotype=biotype)
+    p1=p1+geom_text(data=genes, aes(x=gene_start,y=as.numeric(y),label=gene_symbol),hjust=0, vjust=0, size=label_size)
+    p1=p1+geom_rect(data=shades, mapping=aes(ymax=y2,xmin=x1, xmax=x2, ymin=y1,fill=biotype))+labs(fill="Biotype")
+
+  }
   p1=p1+theme(axis.text.y=element_blank(), axis.title.y=element_blank(),axis.ticks.y=element_blank())
   p1=p1+xlab(paste("Position on chr",chr, sep=""))
 
  # p1=p1+scale_y_discrete(breaks=NULL)
 
-
-  p1=p1+geom_rect(data=shades, mapping=aes(ymax=y2,xmin=x1, xmax=x2, ymin=y1,fill=biotype))+labs(fill="Biotype")
   p1=p1+suppressWarnings(coord_cartesian(xlim=c(xmin, xmax), ylim=c(1,level_count+0.2)))
   if(!is.null(vline)){
     p1=p1+geom_vline(xintercept =vline , colour="grey", linetype="dashed")
